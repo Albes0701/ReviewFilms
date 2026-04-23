51d079ae2400673ecd2e6e43f258609b56a8ae47
Albes0701 <137418865+Albes0701@users.noreply.github.com>
2026-04-23 16:00:54 +0700

    add cors config

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApiControllers();
 builder.Services.AddApiSwagger();
+builder.Services.AddApiCors();
 
 var app = builder.Build();
 
 app.UseGlobalExceptionMiddleware();
 app.UseApiSwagger();
 
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
